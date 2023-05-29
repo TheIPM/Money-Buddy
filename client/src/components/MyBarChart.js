825eb0ff266d7d8d48c2003001a81e559555dd4e
TheIPM <islamluse@gmail.com>
2023-05-29 17:59:37 +0930

    fixed the bar graph

--- client/src/components/MyBarChart.js
+++ client/src/components/MyBarChart.js
@@ -2,17 +2,21 @@ import React from 'react';
 import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend, CartesianGrid } from 'recharts';
 
 const MyBarChart = ({ data }) => {
+ 
+  const formattedData = data.map((entry) => ({
+    ...entry,
+    [entry.type]: entry.value, 
+  }));
 
   return (
-    <BarChart width={600} height={300} data={data}>
+    <BarChart width={600} height={300} data={formattedData}>
       <CartesianGrid strokeDasharray="3 3" />
       <XAxis dataKey="name" />
       <YAxis />
       <Tooltip />
       <Legend />
-      {data.map((entry, index) => (
-        <Bar key={index} dataKey="value" fill={entry.type === 'income' ? '#32CD32' : '#FF0000'} />
-      ))}
+      <Bar dataKey="income" fill="#32CD32" /> 
+      <Bar dataKey="expenses" fill="#FF0000" /> 
     </BarChart>
   );
 };
