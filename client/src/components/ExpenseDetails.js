cb0502867944efcd8d2678452045589a8e85af72
TheIPM <islamluse@gmail.com>
2023-06-01 00:03:54 +0930

    added download options

--- client/src/components/ExpenseDetails.js
+++ client/src/components/ExpenseDetails.js
@@ -1,54 +1,71 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
-import { Card, Table } from 'react-bootstrap';
+import { Button, Card, Table } from 'react-bootstrap';
 import { QUERY_ME } from '../utils/queries';
 import { fromUnixTime, format } from 'date-fns';
 import 'animate.css/animate.min.css';
 
-
 const ExpenseDetails = () => {
-    const { loading, data } = useQuery(QUERY_ME);
-    console.log(data);  
+  const { loading, data } = useQuery(QUERY_ME);
   
-    if (loading) {
-      return <div>Loading...</div>;
-    }
-  
-    if (!data || !data.me || !data.me.expenses) {
-      return <div>No expenses data found for the user</div>;
-    }
-  
-    const { expenses } = data.me;
-  
-    return (
-        <Card className="mt-3 animate__animated animate__fadeIn">
-          <Card.Body>
-            <Card.Title>Expense Entries</Card.Title>
-            <Table striped bordered hover>
-              <thead>
-                <tr>
-                  <th>Description</th>
-                  <th>Amount</th>
-                  <th>Date</th>
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!data || !data.me || !data.me.expenses) {
+    return <div>No expenses data found for the user</div>;
+  }
+
+  const { expenses } = data.me;
+
+  const exportToCSV = () => {
+    let csvContent = 'Description,Amount,Date\n'; // column headers
+    expenses.forEach((expenseItem) => {
+      const date = fromUnixTime(expenseItem.date / 1000);
+      const dateString = format(date, 'MM/dd/yyyy');
+      csvContent += `${expenseItem.description},${expenseItem.amount},${dateString}\n`;
+    });
+
+    const blob = new Blob([csvContent], { type: 'text/csv' });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = 'expenses.csv';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
+  return (
+    <Card className="mt-3 animate__animated animate__fadeIn">
+      <Card.Body>
+        <Card.Title>Expense Entries</Card.Title>
+        <Button onClick={exportToCSV} variant="primary" className="mb-2">Download CSV</Button>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Description</th>
+              <th>Amount</th>
+              <th>Date</th>
+            </tr>
+          </thead>
+          <tbody>
+            {expenses.map((expenseItem, i) => {
+              const date = fromUnixTime(expenseItem.date / 1000);
+              const dateString = format(date, 'MM/dd/yyyy');
+              return (
+                <tr key={i}>
+                  <td>{expenseItem.description}</td>
+                  <td>{expenseItem.amount}</td>
+                  <td>{dateString}</td>
                 </tr>
-              </thead>
-              <tbody>
-                {expenses.map((expenseItem, i) => {
-                  const date = fromUnixTime(expenseItem.date / 1000);
-                  const dateString = format(date, 'MM/dd/yyyy');
-                  return (
-                    <tr key={i}>
-                      <td>{expenseItem.description}</td>
-                      <td>{expenseItem.amount}</td>
-                      <td>{dateString}</td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </Table>
-          </Card.Body>
-        </Card>
-      );
-    };
-    
-    export default ExpenseDetails;
+              );
+            })}
+          </tbody>
+        </Table>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default ExpenseDetails;
--- client/src/components/IncomeDetails.js
+++ client/src/components/IncomeDetails.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
-import { Card, Table } from 'react-bootstrap';
+import { Button, Card, Table } from 'react-bootstrap';
 import { QUERY_ME } from '../utils/queries';
 import { fromUnixTime, format } from 'date-fns';
 import 'animate.css/animate.min.css';
@@ -18,10 +18,29 @@ const IncomeDetails = () => {
 
   const { income } = data.me;
 
+  const exportToCSV = () => {
+    let csvContent = 'Description,Amount,Date\n'; // column headers
+    income.forEach((incomeItem) => {
+      const date = fromUnixTime(incomeItem.date / 1000);
+      const dateString = format(date, 'MM/dd/yyyy');
+      csvContent += `${incomeItem.description},${incomeItem.amount},${dateString}\n`;
+    });
+
+    const blob = new Blob([csvContent], { type: 'text/csv' });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = 'income.csv';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
   return (
     <Card className="mt-3 animate__animated animate__fadeIn">
       <Card.Body>
         <Card.Title>Income Entries</Card.Title>
+        <Button onClick={exportToCSV} variant="primary" className="mb-2">Download CSV</Button>
         <Table striped bordered hover>
           <thead>
             <tr>
