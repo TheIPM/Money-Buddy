3fc0144b003dd7353c665cee4bff85dc532e09e3
TheIPM <islamluse@gmail.com>
2023-05-31 20:39:35 +0930

    added a base readMe

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# Money-Buddy
+# Money-Buddy ![badmath](https://img.shields.io/github/stars/TheIPM/My-Portfolio?style=plastic) ![badmath](https://img.shields.io/github/followers/TheIPM)
+
+## Description
+
+This is my final project that ive made called Money Buddy, the purpose of this is just to put in your income and your expenses, and you can have a visual representation of how it looks like. You can also put in up coming bills and goals, and see how many days till they are due.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+N/A
+
+## Usage
+
+To use this website you just got to follow these following steps:
+1. Create account/Log in
+2. Enter your income or expenses
+3. See the analysis
+4. Enter upcoming bills and goals
+5. Click on profile and see the details of all income and expenses
+
+## Credits
+
+
+## License
+
+N/A
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Navigate, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import Card from 'react-bootstrap/Card';
@@ -6,6 +6,7 @@ import ListGroup from 'react-bootstrap/ListGroup';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 import IncomeDetails from '../components/IncomeDetails';
 import ExpenseDetails from '../components/ExpenseDetails';
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
@@ -14,13 +15,13 @@ import Auth from '../utils/auth';
 
 const Profile = () => {
   const { username: userParam } = useParams();
+  const [showDetails, setShowDetails] = useState(false);
 
   const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
     variables: { username: userParam },
   });
 
   const user = data?.me || data?.user || {};
-  // navigate to personal profile page if username is yours
   if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
     return <Navigate to="/me" />;
   }
@@ -43,12 +44,25 @@ const Profile = () => {
       <Row className="justify-content-md-center">
         <Col xs={12} md={8}>
           <Card className="mt-5">
-            <Card.Header className="text-center font-weight-bold">User Details</Card.Header>
-            <ListGroup variant="flush">
-              <ListGroup.Item>Id: {user._id}</ListGroup.Item>
-              <ListGroup.Item>Email: {user.email}</ListGroup.Item>
-              <ListGroup.Item>Username: {user.username}</ListGroup.Item>
-            </ListGroup>
+            <Card.Header className="text-center font-weight-bold">
+              User Details 
+              <Button
+                className="ml-2"
+                variant="outline-primary"
+                size="sm"
+                onClick={() => setShowDetails(!showDetails)}
+                style={{ marginLeft: '1rem' }} 
+              >
+                {showDetails ? 'Hide' : 'Show'}
+              </Button>
+            </Card.Header>
+            {showDetails && (
+              <ListGroup variant="flush">
+                <ListGroup.Item>Id: {user._id}</ListGroup.Item>
+                <ListGroup.Item>Email: {user.email}</ListGroup.Item>
+                <ListGroup.Item>Username: {user.username}</ListGroup.Item>
+              </ListGroup>
+            )}
             <Card.Body>
               <Card.Title className="text-center font-weight-bold">Income Details</Card.Title>
               <IncomeDetails/>
